2085e5b0e17a1945d51669d8e52e1c937bbe6f61
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-04-10 16:34:24 +0200

    Setup webpack configuration
    In order to check how does webpack bundle modules and read its output i
    had to set up configuration and build modes.

---
+++ yes-module/webpack-bundling-process/src/index.js
@@ -0,0 +1,5 @@
+// import and use only cube from math module
+// then check if harmony-unused-import is present for square function
+import { cube } from './math';
+
+document.body.appendChild(document.createTextNode(`${cube(2)}`));
