5475a37c5f7ac514cd84ef4f34c6c4311f4159c2
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-04-10 16:37:52 +0200

    Compile react
    Check what is included in output when react is compiled and what happens
    with unused react component.

--- yes-module/webpack-bundling-process/src/index.js
+++ yes-module/webpack-bundling-process/src/index.js
@@ -1,5 +1,7 @@
 // import and use only cube from math module
 // then check if harmony-unused-import is present for square function
 import { cube } from './math';
+// check if there is any comment present for unused import
+import { App } from './App';
 
 document.body.appendChild(document.createTextNode(`${cube(2)}`));
