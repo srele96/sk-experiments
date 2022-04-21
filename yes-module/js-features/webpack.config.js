ae97d4e912bce89db5a7a29a98b7bacefcfa475b
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-04-21 18:48:08 +0200

    js-features: update dependencies and runn yarn install
    Package script was working - was able to run webpack, but dependencies
    were missing.

--- yes-module/js-features/webpack.config.js
+++ yes-module/js-features/webpack.config.js
@@ -21,4 +21,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
