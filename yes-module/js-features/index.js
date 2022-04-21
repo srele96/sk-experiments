8f0f3f039eee88a739dd3742452205ade9265f89
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-04-21 18:29:05 +0200

    js-features: wrap two examples, each in IIFE
    In this case named IIFE provides clear intention of code and lessens need for
    comments.

--- yes-module/js-features/index.js
+++ yes-module/js-features/index.js
@@ -3,43 +3,39 @@ const { createRoot } = require('react-dom');
 
 const body = document.getElementsByTagName('body')[0];
 
-/**
- * Write 'useful' example of short circuiting usage...
- */
-
-function getUser() {
-  return {
-    id: 1,
-    displayName: 'Shrek',
-    accountViews: 6,
-  };
-}
-
-function getUserSection() {
-  const user = getUser();
-
-  const displayName = document.createElement('h2');
-  const accountViews = document.createElement('p');
-  const section = document.createElement('section');
-
-  displayName.innerText = user.displayName;
-  accountViews.innerText = `Account views: ${user.accountViews}`;
-  section.appendChild(displayName);
-  section.appendChild(accountViews);
-
-  return section;
-}
-
-function getPermissionStatus(hasPermission) {
-  const p = document.createElement('h2');
-  p.innerText = `User ${
-    hasPermission ? 'has ' : "doesn't have "
-  } required permission`;
-
-  return p;
-}
-
 (function shortCircuitingInRegularJavascript() {
+  function getUser() {
+    return {
+      id: 1,
+      displayName: 'Shrek',
+      accountViews: 6,
+    };
+  }
+
+  function getUserSection() {
+    const user = getUser();
+
+    const displayName = document.createElement('h2');
+    const accountViews = document.createElement('p');
+    const section = document.createElement('section');
+
+    displayName.innerText = user.displayName;
+    accountViews.innerText = `Account views: ${user.accountViews}`;
+    section.appendChild(displayName);
+    section.appendChild(accountViews);
+
+    return section;
+  }
+
+  function getPermissionStatus(hasPermission) {
+    const p = document.createElement('h2');
+    p.innerText = `User ${
+      hasPermission ? 'has ' : "doesn't have "
+    } required permission`;
+
+    return p;
+  }
+
   const h1 = document.createElement('h1');
   h1.innerText = 'User profile page';
 
@@ -55,88 +51,85 @@ function getPermissionStatus(hasPermission) {
 
 /**
  * Animate downloading progress bar that displays:
- *  - Total file size
- *  - Downloaded size
- *  - Current download speed
- *  - Imitate changing download speed, no internet is stable
+ *  - Total file size.
+ *  - Downloaded size.
+ *  - Current download speed.
+ *  - Imitate changing download speed, no internet is stable.
  */
+(function downloadProgressBarExample() {
+  const e = createElement;
 
-const e = createElement;
+  const startAnimation = (function () {
+    let _delay;
+    let _running = true;
 
-const startAnimation = (function () {
-  let _delay;
-  let _running = true;
+    function animate(callback) {
+      requestAnimationFrame(() => {
+        callback();
 
-  function animate(callback) {
-    requestAnimationFrame(() => {
-      callback();
+        if (_running) setTimeout(() => animate(callback), _delay);
+      });
+    }
 
-      if (_running) setTimeout(() => animate(callback), _delay);
-    });
-  }
+    function cleanup() {
+      _running = false;
+    }
 
-  function cleanup() {
-    _running = false;
-  }
+    /**
+     * Initially sets callback on requestAnimationFrame call stack.
+     *
+     * Afterwards sets timeout to call new requestAnimationFrame after delay.
+     *
+     * @param {() => void} callback - Function executed on requestAnimationFrame.
+     * @param {number} delay - Time between setTimeout and requestAnimationFrame
+     * in milliseconds.
+     *
+     * @return {() => void} - Cleanup function, terminates onward execution.
+     */
+    return function startAnimation(callback, delay = 500) {
+      _delay = delay;
+      animate(callback);
 
-  /**
-   * Initially sets callback on requestAnimationFrame call stack.
-   *
-   * Afterwards sets timeout to call new requestAnimationFrame after delay.
-   *
-   * @param {() => void} callback - Function executed on requestAnimationFrame.
-   * @param {number} delay - Time between setTimeout and requestAnimationFrame
-   * in milliseconds.
-   *
-   * @return {() => void} - Cleanup function, terminates onward execution.
-   */
-  return function startAnimation(callback, delay = 500) {
-    _delay = delay;
-    animate(callback);
+      return cleanup;
+    };
+  })();
 
-    return cleanup;
-  };
-})();
-
-function AnimateDownloadingProgressBar() {
-  return e(
-    'div',
-    { style: { width: 'min-content' } },
-    e(
+  function AnimateDownloadingProgressBar() {
+    return e(
       'div',
-      {
-        style: {
-          width: '300px',
-          height: '50px',
-          border: '5px solid black',
-        },
-      },
+      { style: { width: 'min-content' } },
       e(
         'div',
         {
           style: {
-            width: '18%',
-            height: '100%',
-            backgroundColor: 'black',
+            width: '300px',
+            height: '50px',
+            border: '5px solid black',
           },
         },
-        null
-      )
-    ),
-    e('p', { style: { textAlign: 'center' } }, 'Downloaded 18% of 187mb'),
-    e('p', { style: { textAlign: 'center' } }, 'Download speed 378kb/s')
-  );
-}
+        e(
+          'div',
+          {
+            style: {
+              width: '18%',
+              height: '100%',
+              backgroundColor: 'black',
+            },
+          },
+          null
+        )
+      ),
+      e('p', { style: { textAlign: 'center' } }, 'Downloaded 18% of 187mb'),
+      e('p', { style: { textAlign: 'center' } }, 'Download speed 378kb/s')
+    );
+  }
 
-(function setStyle() {
   const style = document.createElement('style');
   style.innerText = '*{ box-sizing: border-box; }';
 
   const head = document.getElementsByTagName('head')[0];
   head.appendChild(style);
-})();
 
-(function renderAnimationApp() {
   const root = document.createElement('div');
   root.id = 'root';
 
