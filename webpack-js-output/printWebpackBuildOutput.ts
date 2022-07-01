c7a171587dd57012ed5a91e3f5d2e4ed7e487a65
srele96 <srele96@gmail.com>
2022-07-01 21:07:18 +0200

    Added configuration and entry file
    I see the two printed file names in the console after compilation.
    The two printed file names match the files in /dist.
    Ignored dist directory from VCS because they are compiled files.

--- webpack-js-output/printWebpackBuildOutput.ts
+++ webpack-js-output/printWebpackBuildOutput.ts
@@ -1,6 +1,32 @@
-import webpack from 'webpack';
+import webpack, { Configuration } from 'webpack';
+import { join } from 'path';
 
-webpack({}, (error, stats) => {
+const config: Configuration = {
+  mode: 'development',
+  // Don't wanna setup ts for the app code. I want typed stats to know what I
+  // can expect.
+  entry: require.resolve('./src/index.js'),
+  output: {
+    clean: true,
+    path: join(__dirname, 'dist'),
+    // Split every other directory to check do I receive file paths or just
+    // the file names.
+    filename: (pathData) => {
+      return pathData.chunk.name === 'main' ? '[name].js' : 'assets/[name].js';
+    },
+  },
+  // I want to split chunks as much as possible for the sake of seeing the
+  // files in stats.
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+};
+
+// Read the js output files from webpack stats, build or wherever.
+webpack(config, (error, stats) => {
   if (error) console.log('Error during Webpack compilation', error);
   else {
     // here we can read js output files and create our own html template
---
+++ webpack-js-output/src/index.js
@@ -0,0 +1,2 @@
+// the sample code that gets compiled goes here
+console.log('How does this even work?');
