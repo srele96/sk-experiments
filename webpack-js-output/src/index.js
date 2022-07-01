2fee73415b33cae626aeeaf7098ca48e45d6d471
srele96 <srele96@gmail.com>
2022-07-01 21:10:34 +0200

    Added more app files
    More files will produce more output files. I wanted to have more files
    to see how do their names look in the stats object. For example, their
    names could be different in the stats object if they have hash.

--- webpack-js-output/src/index.js
+++ webpack-js-output/src/index.js
@@ -1,2 +1,16 @@
 // the sample code that gets compiled goes here
-console.log('How does this even work?');
+import('./foo').then((value) => {
+  console.log(value.default());
+});
+
+import('./baz').then((value) => {
+  console.log(value.default());
+});
+
+import { bar } from './bar';
+console.log(bar());
+
+import { createElement as e } from 'react';
+import { createRoot } from 'react-dom/client';
+import Qux from './Qux';
+createRoot(document.getElementById('qux')).render(e(Qux));
