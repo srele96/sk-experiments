e3958edd5416a0104dded7388af389c11ad6bdc1
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-07-30 13:32:31 +0200

    refactor(new): create directory from template
    * refactor code that read from template directory and wrote the files to
      directory user provides
    * remove template directory, use template-package.json and write it to
      directory user provides
    * write empty yarn.lock to directory user provides

--- experiments/new/index.js
+++ experiments/new/index.js
@@ -1,110 +1,71 @@
 #!/usr/bin/env node
 'use strict';
 
-const { readFile, mkdir, writeFile } = require('fs');
-const { join } = require('path');
+const { readFile, mkdir, writeFile } = require('fs/promises');
+const { join, basename } = require('path');
 const { Command } = require('commander');
 const program = new Command();
 const { name, description, version } = require('./package.json');
 
 program.name(name).description(description).version(version);
 
-/**
- *
- * @param {string} filePath Path to a file to read
- * @returns {Promise<string>} File content
- */
-function getFileContent(filePath) {
-  return new Promise((resolve, reject) => {
-    if (typeof filePath !== 'string')
-      reject(`Expected filePath to be type of string, received ${filePath}`);
-    if (filePath === '') reject('filePath cannot be an empty string');
-
-    readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
-      if (err) reject(err);
-      else resolve(data);
-    });
-  });
-}
-
-/**
- * @param {string} name For package.json
- * @param {string} description For package.json
- */
-function getPackageJsonWith(name = '', description = '') {
-  if (typeof name !== 'string')
-    throw new Error(`Expected name to be type of string, received ${name}`);
-  if (typeof description !== 'string')
-    throw new Error(
-      `Expected description to be type of string, received ${description}`
-    );
-
-  return getFileContent(join(__dirname, 'template', 'package.json')).then(
-    (content) => {
-      const placeholder = { name: '{name}', description: '{description}' };
-
-      return content
-        .replace(placeholder.name, name)
-        .replace(placeholder.description, description);
-    }
-  );
-}
-
-/**
- * @param {string} path A name for directory.
- * @return {Promise<string>} Created directory path.
- */
-function mkdirAsync(path) {
-  return new Promise((resolve, reject) => {
-    if (typeof path !== 'string')
-      reject(`Expected path to be type of string, received ${path}`);
-    if (path === '') reject('path cannot be an empty string');
-
-    mkdir(path, { recursive: true }, (err) => {
-      if (err) reject(err);
-      else resolve();
-    });
-  });
-}
-
-/**
- * @param {string} path
- * @param {string} content
- * @return {Promise<void>}
- */
-function writeFileAsync(path, content) {
-  return new Promise((resolve, reject) => {
-    writeFile(path, content, 'utf-8', (err) => {
-      if (err) reject(err);
-      else resolve();
-    });
-  });
-}
-
 program
   .argument('<directory>', 'new directory for an experiment')
   .option('-d, --description <description>', 'experiment description', '')
   .action((directory, { description }) => {
-    mkdirAsync(directory)
-      .then(() => {
-        // write package.json and yarn.lock
-        const writeFiles = [
-          getPackageJsonWith(directory, description).then((packageJson) => {
-            return writeFileAsync(join(directory, 'package.json'), packageJson);
-          }),
-          getFileContent(join(__dirname, 'template', 'yarn.lock')).then(
-            (yarnLock) => {
-              return writeFileAsync(join(directory, 'yarn.lock'), yarnLock);
-            }
-          ),
-        ];
+    // user may provide dirname or path which
+    // results in invalid name for package.json
+    const baseDirname = basename(directory);
 
-        return Promise.all(writeFiles);
-      })
+    // create directory
+    mkdir(directory)
       .then(() => {
-        console.log(`Created ${directory}`);
+        console.log('Created ' + baseDirname);
+
+        // read package.json template
+        readFile(join(__dirname, 'template-package.json'), 'utf-8')
+          .then((packageJson) => {
+            // replace placeholders with user input
+            const namePlaceholder = '{name}';
+            const descriptionPlaceholder = '{description}';
+
+            return packageJson
+              .replace(namePlaceholder, baseDirname)
+              .replace(descriptionPlaceholder, description);
+          })
+          .then((packageJson) => {
+            // write package.json to created directory
+            return writeFile(
+              join(directory, 'package.json'),
+              packageJson,
+              'utf-8'
+            );
+          })
+          .then(() => {
+            console.log('Created ' + join(baseDirname, 'package.json'));
+          })
+          .catch((error) => {
+            console.log(
+              'Error creating ' + join(baseDirname, 'package.json'),
+              error
+            );
+          });
+
+        // write empty yarn.lock to created directory
+        writeFile(join(directory, 'yarn.lock'), '', 'utf-8')
+          .then(() => {
+            console.log('Created ' + join(baseDirname, 'yarn.lock'));
+          })
+          .catch((error) => {
+            console.error(
+              'Error creating ' + join(baseDirname, 'yarn.lock'),
+              error
+            );
+          });
       })
-      .catch(console.error);
+      .catch((error) => {
+        console.error('Error creating ' + baseDirname, error);
+      });
   });
 
 program.parse();
