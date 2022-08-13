24f2e22003bd88af6ae34e68cc5a184fb2914050
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-08-13 12:27:19 +0200

    refactor(streams): update comments in the code
    * remove non-valuable comments from the code because they exist in
      markdown file
    * add comments to show code intention

--- streams/how-streams-transfer-data.js
+++ streams/how-streams-transfer-data.js
@@ -2,11 +2,13 @@ const { createWriteStream, createReadStream } = require('fs');
 const { Readable } = require('stream');
 const { join } = require('path');
 
+// push alphabet characters to readable stream
 const rs = new Readable({
   read: (function () {
     let alphabetChar = 97;
 
     return function () {
+      // terminate after last character
       if (alphabetChar > 'z'.charCodeAt(0)) rs.push(null);
       else rs.push(String.fromCharCode(alphabetChar));
       alphabetChar += 1;
@@ -27,19 +29,9 @@ rs.pipe(createWriteStream(getPath('output.txt')));
 // Transfer executable using streams.
 // Execute the transferred executable.
 //
-// What does this example achieve and prove?
-// - Streams can transfer any kind of data (.exe, .txt, .zip, etc...)
-// - Computers transfer data using streams
-// - Streams don't care about the type of data they work with.
-// - Humans make data type differentiation, but for computers all
-//   of it is 0 and 1
-//
-// Last step
-//
 // If the copied executable is successfully executed, then
 // the stream has successfully transferred the executable.
 const source = createReadStream('./main.exe');
-// streaming one source to two destinations is called teeing
 source.pipe(createWriteStream(getPath('main-copy.exe')));
 // make executable content readable
 source.pipe(createWriteStream(getPath('main-copy.txt')));
