19f81940b500fa317c5b4061bcec8ac73d1e0831
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-08-14 01:34:29 +0200

    feat(cpp): add strict compiling flag

--- cpp/run.bat
+++ cpp/run.bat
@@ -1,3 +1,3 @@
 @REM use suggested strict flags for compilation
 @REM https://github.com/cpp-best-practices/cppbestpractices/blob/master/02-Use_the_Tools_Available.md#gcc--clang
-clang++ -Wall -Wextra -Wshadow -Weffc++ -Wnon-virtual-dtor -pedantic main.cpp -o main.exe && main
+clang++ -Wall -Wextra -Werror -Wshadow -Weffc++ -Wnon-virtual-dtor -pedantic main.cpp -o main.exe && main
