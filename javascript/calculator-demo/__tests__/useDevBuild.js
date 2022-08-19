f49b7e80eed6bbe5f7571255e663103a146e88ff
Srecko <srele96@gmail.com>
2022-08-19 19:08:19 +0200

    feat(calculator-demo): add tests
    * Test named exports. Previous tests verified that default export
      worked, but it didn't test named exports.
    * Add utility function that runs same set of tests for default and
      named exports for dev and prod builds.

--- javascript/calculator-demo/__tests__/useDevBuild.js
+++ javascript/calculator-demo/__tests__/useDevBuild.js
@@ -1,10 +1,14 @@
 const calculator = require('../dist/calculator.development');
+const {
+  add,
+  subtract,
+  multiply,
+  divide,
+} = require('../dist/calculator.development');
+const { runCalculatorTests } = require('./util/runCalculatorTests');
 
-function ifConsolePrintsCorrectResultsItWorks() {
-  console.log('1 + 1 = ' + calculator.add(1, 1));
-  console.log('5 - 3 = ' + calculator.subtract(5, 3));
-  console.log('5 * 3 = ' + calculator.multiply(5, 3));
-  console.log('10 / 5 = ' + calculator.divide(10, 5));
-}
-
-ifConsolePrintsCorrectResultsItWorks();
+runCalculatorTests(calculator, 'default export - development build');
+runCalculatorTests(
+  { add, subtract, multiply, divide },
+  'named export - development build'
+);
