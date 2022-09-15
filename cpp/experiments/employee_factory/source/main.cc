fdc63754c3cd648da8db51ef2974005f05ce7903
Srecko <srele96@gmail.com>
2022-09-15 02:16:56 +0200

    verify that employees are working
    After implementing employee roles, verify that they work. I was
    trying to speed up the implementation. I wasn't sure if I missed
    anything, but everything should be fine. After running the sample
    everything appears alright.

--- cpp/experiments/employee_factory/source/main.cc
+++ cpp/experiments/employee_factory/source/main.cc
@@ -1,3 +1,21 @@
 #include <iostream>
 
-int main() { std::cout << "employee_factory"; }
+#include "janitor.h"
+#include "maintenance.h"
+#include "operator.h"
+#include "tester.h"
+
+int main() {
+  Janitor john{"John", "Doe"};
+  Tester rob{"Rob", "Robinson"};
+  Maintenance chris{"Chris", "Crimson"};
+  Operator jane{"Jane", "Doe"};
+
+  std::cout << "---------------------------------------\n";
+  std::cout << "Verify that employee roles are working.\n";
+  std::cout << "---------------------------------------\n";
+  std::cout << john.Work() << "\n";
+  std::cout << rob.Work() << "\n";
+  std::cout << chris.Work() << "\n";
+  std::cout << jane.Work() << "\n";
+}
