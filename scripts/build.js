b0ffff8e340503714d1239e3cf51ab105c863705
Srecko Kostic <s.kostic@netconomy.net>
2022-09-17 21:46:28 +0200

    make directory to stop build from failing

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,5 @@
 const { spawn } = require("child_process");
+const { mkdir } = require("fs");
 
 function verifyThatCommandWorksOnVercelDeployment() {
   spawn("git ls-tree -r --name-only HEAD | tree --fromfile", {
@@ -8,3 +9,9 @@ function verifyThatCommandWorksOnVercelDeployment() {
 }
 
 verifyThatCommandWorksOnVercelDeployment();
+
+// make directory to stop build from failing
+mkdir(__dirname, "build", (error, path) => {
+  if (error) throw error;
+  console.log(`Created directory: ${path}`);
+});
