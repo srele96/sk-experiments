63ad6fc24f3b85f9848bda5ec0df44de9eb99faa
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-09-23 00:32:21 +0200

    add reference to post about rvalue ref vs value
    * Save for later read
    
    * Explain implementation decision reason
    
    Developer Contract:
      - I will mutate whatever you give me.
      - I accept only rvalue. For example, an anonymous instance or
        explicit std::move
    
    Result:
      - Error when passing lvalue
      - I expect a move-only type
      - Explicit mutability on the place of usage

--- cpp/experiments/employee_factory/source/facility/factory.h
+++ cpp/experiments/employee_factory/source/facility/factory.h
@@ -13,6 +13,7 @@ class Factory {
   std::vector<std::unique_ptr<role::Employee>> employees{};
 
  public:
+  // http://scottmeyers.blogspot.com/2014/07/should-move-only-types-ever-be-passed.html
   void Accept(std::unique_ptr<role::Employee>&& employee);
   void StartWorkHours();
 };
