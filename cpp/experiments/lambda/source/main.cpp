11f2b7309beac9bb4d3fbfb63b03e93c87542d5d
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-10-18 22:43:05 +0200

    feat: learn lambda (#128)
    * I didn't know how to write and use lambda in C++
    * Learn to write and use lambda using C++
    * Implement linear algebra coordinate
    
    Get 2D coordinates from a 3D vector
    I had to learn how to use lambda to draw vectors in the terminal to implement my solution.

---
+++ cpp/experiments/lambda/source/main.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+#include "linear_algebra.hpp"
+
+int main() {
+  const linear_algebra::position CENTER{3, 3};
+  linear_algebra::coordinate coordinate{CENTER};
+
+  coordinate.print_formatted_state("Before movement:");
+
+  std::vector<int> destination{2, 2, -1};
+
+  coordinate.move_and_print_movements(destination);
+  coordinate.print_formatted_state("After movement:");
+
+  return 0;
+}
