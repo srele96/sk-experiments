9c77b1bd6a39e85dd47145c9d1fd06392e98c6c2
srele96 <srele96@gmail.com>
2022-10-18 22:32:24 +0200

    rename 'vector_v' to 'destination'
    Make code more readable.

--- cpp/experiments/lambda/source/main.cpp
+++ cpp/experiments/lambda/source/main.cpp
@@ -4,14 +4,14 @@
 
 int main() {
   const linear_algebra::position CENTER{3, 3};
-  linear_algebra::coordinate coordinate_v{CENTER};
+  linear_algebra::coordinate coordinate{CENTER};
 
-  coordinate_v.print_formatted_state("Before movement:");
+  coordinate.print_formatted_state("Before movement:");
 
-  std::vector<int> vector_v{2, 2, -1};
+  std::vector<int> destination{2, 2, -1};
 
-  coordinate_v.move_and_print_each(vector_v);
-  coordinate_v.print_formatted_state("After movement:");
+  coordinate.move_and_print_each(destination);
+  coordinate.print_formatted_state("After movement:");
 
   return 0;
 }
