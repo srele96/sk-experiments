862a463d3e49946188121fc0100766ee52eb3af8
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-10-22 01:42:44 +0200

    replace magic constants with structs

---
+++ cpp/experiments/linear_algebra/display_vectors/src/display_vectors/axis.hpp
@@ -0,0 +1,25 @@
+#ifndef DISPLAY_VECTORS_DISPLAY_VECTORS_AXIS_HPP
+#define DISPLAY_VECTORS_DISPLAY_VECTORS_AXIS_HPP
+
+namespace display_vectors {
+namespace axis {
+
+struct _1d {
+  int x;
+};
+
+struct _2d {
+  int x;
+  int y;
+};
+
+struct _3d {
+  int x;
+  int y;
+  int z;
+};
+
+};  // namespace axis
+};  // namespace display_vectors
+
+#endif  // DISPLAY_VECTORS_DISPLAY_VECTORS_AXIS_HPP
