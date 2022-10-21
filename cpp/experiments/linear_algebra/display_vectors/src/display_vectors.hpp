757bcb47ef40f66973df106ffcd8c15d520c60e0
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-10-22 01:42:44 +0200

    replace magic constants with structs

--- cpp/experiments/linear_algebra/display_vectors/src/display_vectors.hpp
+++ cpp/experiments/linear_algebra/display_vectors/src/display_vectors.hpp
@@ -1,6 +1,7 @@
 #ifndef DISPLAY_VECTORS_DISPLAY_VECTORS_HPP
 #define DISPLAY_VECTORS_DISPLAY_VECTORS_HPP
 
+#include "display_vectors/axis.hpp"
 #include "display_vectors/coordinate.hpp"
 #include "display_vectors/emulate_gui.hpp"
 #include "display_vectors/origin.hpp"
