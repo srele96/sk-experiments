177aa1d8db1f1f3b57e5eca8929c968bbdc8d15c
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-10-22 01:07:09 +0200

    add missing coordinate implementation
    I was sick and tired so i abandoned the work. Now that I came back I
    have to fix the implementation and bug. Great.

--- cpp/experiments/linear_algebra/display_vectors/src/display_vectors/impl/coordinate.cpp
+++ cpp/experiments/linear_algebra/display_vectors/src/display_vectors/impl/coordinate.cpp
@@ -1,17 +1,27 @@
 #include "../coordinate.hpp"
 
+#include <functional>
 #include <vector>
 
 display_vectors::coordinate::coordinate(const std::vector<int>& vector)
     : position{display_vectors::point{4, 4}},
-      move_along_axis{{
-          [this](const int& move_by) {},
-      }} {}
+      move_along_axis{
+          {
+              [this](const int& move_by) { position.x += move_by; },
+              [this](const int& move_by) { position.y -= move_by; },
+              [this](const int& move_by) {
+                position.x -= move_by;
+                position.y += move_by;
+              },
+          },
+      } {
+  set_position(vector);
+}
 
 void display_vectors::coordinate::set_position(const std::vector<int>& vector) {
   for (std::vector<int>::size_type i = 0; i != vector.size(); ++i) {
     int component = vector.at(i);
-    auto move_point_by = move_along_axis.at(i);
+    std::function<void(const int&)> move_point_by = move_along_axis.at(i);
     move_point_by(component);
   }
 }
