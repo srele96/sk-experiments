872ff3d5544a64bee6b8d72218f3b95153663fb0
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-10-26 03:32:38 +0200

    add test if the two sum returns correct indices

--- javascript/experiments/algorithms/src/__tests__/two_sum.test.js
+++ javascript/experiments/algorithms/src/__tests__/two_sum.test.js
@@ -1,3 +1,7 @@
 const { two_sum } = require('../two_sum');
 
-
+test('two sum returns correct indices', () => {
+  expect(two_sum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+  expect(two_sum([3, 2, 4], 6)).toEqual([1, 2]);
+  expect(two_sum([3, 3], 6)).toEqual([0, 1]);
+});
