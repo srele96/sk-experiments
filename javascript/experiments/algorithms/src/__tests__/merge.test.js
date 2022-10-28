d039e582e53fcaffd3e2b8c91a0e6d14bd4369ac
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-10-28 05:22:11 +0200

    replace magic variables with named constants

--- javascript/experiments/algorithms/src/__tests__/merge.test.js
+++ javascript/experiments/algorithms/src/__tests__/merge.test.js
@@ -1,45 +1,49 @@
 const { merge } = require('../merge');
 
-test('merges two arrays correctly with positive values', () => {
+test('merge two arrays correctly with positive values', () => {
   const nums1 = [1, 2, 3, 0, 0, 0];
   const m = 3;
   const nums2 = [2, 5, 6];
   const n = 3;
+  const merged_and_sorted = [1, 2, 2, 3, 5, 6];
 
   merge(nums1, m, nums2, n);
 
-  expect(nums1).toEqual([1, 2, 2, 3, 5, 6]);
+  expect(nums1).toEqual(merged_and_sorted);
 });
 
-test('merges two arrays correctly with empty second array', () => {
+test('merge two arrays correctly with empty second array', () => {
   const nums1 = [1];
   const m = 1;
   const nums2 = [];
   const n = 0;
+  const merged_and_sorted = [1];
 
   merge(nums1, m, nums2, n);
 
-  expect(nums1).toEqual([1]);
+  expect(nums1).toEqual(merged_and_sorted);
 });
 
-test('merges two arrays correctly with empty first array', () => {
+test('merge two arrays correctly with empty first array', () => {
   const nums1 = [0];
   const m = 0;
   const nums2 = [5];
   const n = 1;
+  const merged_and_sorted = [5];
 
   merge(nums1, m, nums2, n);
 
-  expect(nums1).toEqual([5]);
+  expect(nums1).toEqual(merged_and_sorted);
 });
 
-test('merges two arrays correctly with a negative value', () => {
+test('merge two arrays correctly with a negative value', () => {
   const nums1 = [-5, 10, 15, 20, 0];
   const m = 4;
   const nums2 = [-100];
   const n = 1;
+  const merged_and_sorted = [-100, -5, 10, 15, 20];
 
   merge(nums1, m, nums2, n);
 
-  expect(nums1).toEqual([-100, -5, 10, 15, 20]);
+  expect(nums1).toEqual(merged_and_sorted);
 });
--- javascript/experiments/algorithms/src/__tests__/two_sum.test.js
+++ javascript/experiments/algorithms/src/__tests__/two_sum.test.js
@@ -1,7 +1,25 @@
 const { two_sum } = require('../two_sum');
 
-test('two sum returns correct indices', () => {
-  expect(two_sum([2, 7, 11, 15], 9)).toEqual([0, 1]);
-  expect(two_sum([3, 2, 4], 6)).toEqual([1, 2]);
-  expect(two_sum([3, 3], 6)).toEqual([0, 1]);
+test('should return the correct indices for case 1', () => {
+  const numbers = [2, 7, 11, 15];
+  const target = 9;
+  const indices = [0, 1];
+
+  expect(two_sum(numbers, target)).toEqual(indices);
+});
+
+test('should return the correct indices for case 2', () => {
+  const numbers = [3, 2, 4];
+  const target = 6;
+  const indices = [1, 2];
+
+  expect(two_sum(numbers, target)).toEqual(indices);
+});
+
+test('should return the correct indices for case 3', () => {
+  const numbers = [3, 3];
+  const target = 6;
+  const indices = [0, 1];
+
+  expect(two_sum(numbers, target)).toEqual(indices);
 });
