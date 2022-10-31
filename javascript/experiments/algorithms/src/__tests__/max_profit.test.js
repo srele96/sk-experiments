821127d2783b92f9d5c7e0a8af8774bf3c65a9b1
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-10-31 16:12:03 +0100

    use describe to improve test report readability

--- javascript/experiments/algorithms/src/__tests__/max_profit.test.js
+++ javascript/experiments/algorithms/src/__tests__/max_profit.test.js
@@ -1,23 +1,25 @@
 const { max_profit } = require('../max_profit');
 
-test('should calculate the correct profit when there is one', () => {
-  const case_one = {
-    prices: [7, 1, 5, 3, 6, 4],
-    profit: 5,
-  };
+describe('max_profit', () => {
+  test('should calculate the correct profit when there is one', () => {
+    const case_one = {
+      prices: [7, 1, 5, 3, 6, 4],
+      profit: 5,
+    };
 
-  const case_two = {
-    prices: [2, 4, 1],
-    profit: 2,
-  };
+    const case_two = {
+      prices: [2, 4, 1],
+      profit: 2,
+    };
 
-  expect(max_profit(case_one.prices)).toEqual(case_one.profit);
-  expect(max_profit(case_two.prices)).toEqual(case_two.profit);
-});
+    expect(max_profit(case_one.prices)).toEqual(case_one.profit);
+    expect(max_profit(case_two.prices)).toEqual(case_two.profit);
+  });
 
-test('should return zero profit when there is none', () => {
-  const prices = [7, 6, 4, 3, 1];
-  const profit = 0;
+  test('should return zero profit when there is none', () => {
+    const prices = [7, 6, 4, 3, 1];
+    const profit = 0;
 
-  expect(max_profit(prices)).toEqual(profit);
+    expect(max_profit(prices)).toEqual(profit);
+  });
 });
