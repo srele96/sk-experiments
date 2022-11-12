b062b9aca7a7d83355641c2d224e4f5c8871a1ac
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-11-12 17:14:34 +0100

    use swc to compile javascript

--- javascript/experiments/layers/webpack.config.js
+++ javascript/experiments/layers/webpack.config.js
@@ -4,6 +4,18 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: path.resolve('src', 'index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: {
+          // Make sure the loader is installed.
+          loader: require.resolve('swc-loader'),
+        },
+      },
+    ],
+  },
   devServer: {
     hot: true,
   },
