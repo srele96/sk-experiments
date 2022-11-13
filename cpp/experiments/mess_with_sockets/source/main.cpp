83d4536a8aeb3b5e89664aa93fa25c66625ab81d
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-11-13 20:14:59 +0100

    add comments to explain the structure

--- cpp/experiments/mess_with_sockets/source/main.cpp
+++ cpp/experiments/mess_with_sockets/source/main.cpp
@@ -2,6 +2,18 @@
 
 #include <iostream>
 
+// The application has to call the WSAStartup(); before using sockets.
+// We have to release the resources at the end by calling WSACleanup();
+//
+// The pattern of using sockets in C++ is:
+//
+//  1. Create type of connection. TCP IPv4
+//  2. Create socket using the type of connection.
+//  3. Bind socket.
+//  4. Listen to the socket.
+//  5. Accept connections from the socket.
+//  6. Write a response to the accepted socket.
+//  7. Close the socket after usage to release the resources.
 int main() {
   WSADATA wsaData;
   const int two = 2;
