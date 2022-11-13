a4853aefafc4ebafb9eb2956276df5015391f9db
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-11-13 20:53:09 +0100

    close socket on the server

--- cpp/experiments/mess_with_sockets/source/main.cpp
+++ cpp/experiments/mess_with_sockets/source/main.cpp
@@ -42,6 +42,7 @@ int main() {
   std::cout << "Listening for incomming connections on the server socket.\n";
 
   // Release the resources.
+  closesocket(server);
   WSACleanup();
   std::cout << "Released server socket resources.\n";
   return 0;
