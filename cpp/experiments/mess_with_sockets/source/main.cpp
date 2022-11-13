2089d878f6d680e0118094a0bc300357b3a9b5cb
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-11-13 20:11:22 +0100

    add comment to provide context

--- cpp/experiments/mess_with_sockets/source/main.cpp
+++ cpp/experiments/mess_with_sockets/source/main.cpp
@@ -6,6 +6,7 @@ int main() {
   WSADATA wsaData;
   const int two = 2;
   const int version_2_2 = MAKEWORD(two, two);
+  // https://learn.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-wsastartup#remarks
   WSAStartup(version_2_2, &wsaData);
 
   // Release the resources.
