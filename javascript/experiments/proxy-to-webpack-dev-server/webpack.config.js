968a6008d1733cf2310c30d2d80d8913313708bf
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-11-22 23:51:12 +0100

    add fast refresh for react

--- javascript/experiments/proxy-to-webpack-dev-server/webpack.config.js
+++ javascript/experiments/proxy-to-webpack-dev-server/webpack.config.js
@@ -2,12 +2,44 @@ const path = require('path');
 
 /** @type {import('webpack').Configuration} */
 const config = {
+  // Don't care to change mode. This is demo.
+  mode: 'development',
   entry: 'client.js',
   // Use the same directory as the express server.
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: '.js',
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+            options: {
+              presets: [
+                '@babel/preset-env',
+                [
+                  '@babel/preset-react',
+                  {
+                    runtime: 'automatic',
+                    mode: 'development',
+                  },
+                ],
+              ],
+              plugins: [require.resolve('react-refresh/babel')],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+  },
+  plugins: [isDevelopment && new ReactRefreshWebpackPlugin()].filter(Boolean),
 };
 
 module.exports = config;
