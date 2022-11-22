e291610c5ed06f54ac18ac26ffde20d38e14c7f1
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-11-22 22:07:51 +0100

    implement search matrix pseudo code

--- cpp/experiments/algorithms/src/search_matrix.cpp
+++ cpp/experiments/algorithms/src/search_matrix.cpp
@@ -1,131 +1,85 @@
 /*
 
+/*
 
-
-find(grid, row_begin, row_end, column_begin, column_end, value):
-  if begin of rows is smaller than the end of rows
-    take value between begin and end of rows
-
-    if value is at the begin of this row or value is at the end of this row
-      found it
-    else if value is above this row
-      search up
-    else if value is below this row
-      search below
-    else
-      if begin of columns is smaller than the end of columns
-        take value between begin and end of columns
-
-        if the value between begin and end is same as searched value
-          found it
-        else if value is higher
-          search right
-        else search left
-      else
-        value not found
-  else
-    value not found
-
-
+target_location = 'found' | 'to_begin' | 'to_end'
+target_not_found = -1
 
 binary_search(begin, end, predicate):
-  if begin smaller than end
-    calculate half between begin and end
+  if begin <= end
+    half = floor(( begin + end ) / 2)
 
-    result = predicate(pass the half)
-
-    if result is found
-    elif result is less
-    elif result is greater
-    else i didn't expect to get here, incorrect return value
-  else
-    value not found
-
-
-
-row = binary_search(row_begin, row_end, find_row)
-if row
-  col = binary_search(col_begin, col_end, find_col)
-  if col
-    value found
-  else
-    value not found
-else:
-  value not found
-
-
-
-
-
-binary_search(begin, end, predicate)
-  if begin < end
-    half = floor ( ( begin + end ) / 2 )
     result = predicate(half)
 
-    if result == 0 // result is found
-      return true // return something to indicate that the result is found
-    elif result == 1 // result is value smaller than the value on half
-      binary_search(begin, half - 1, predicate) // search from begin to half
-    elif result is value larger than the value on half
-      binary_search(half + 1, end, predicate) // search from half to end
-    else
-      error, predicate returned unexpected value
+    switch (result):
+      case target_location.found
+        return half
+      case target_location.to_begin
+        return binary_search(begin, half - 1, predicate)
+      case target_location.to_end
+        return binary_search(half + 1, end, predicate)
+      default
+        Error. Unexpected target location.
   else
-    return false // return something to indicate that the result is not found
+    return target_not_found
 
-find_value(half)
-  if value == numbers[half]
-    return value found
-  elif value < numbers[half]
-    return search from begin to half
-  else
-    return search from half to end
+find_row(row_half):
+  // Check if the target is in current row.
+  if target >= matrix[row_half][col_begin] and
+     target <= matrix[row_half][col_end]
+    return target_location.found
+  if target <= matrix[row_half][col_begin]
+    return target_location.to_begin return
+  target_location.to_end
 
-value = binary_search(begin, end, find_half)
-if value
-  value found
-else
-  value not found
+find_col(col_half):
+  if target === matrix[row][col_half]
+    return target_location.found
+  if target <= matrix[row][col_half]
+    return target_location.to_begin
+  return target_location.to_end
 
 */
+
+* /
 #include <cmath>
 #include <functional>
 #include <iostream>
 #include <stdexcept>
 #include <vector>
 
-namespace search_matrix {
+    namespace search_matrix {
 
-enum class value { found, to_begin, to_end };
+  enum class value { found, to_begin, to_end };
 
-bool binary_search(int begin, int end,
-                   const std::function<value(int)>& predicate) {
-  if (begin < end) {
-    const int half = std::floor((begin + end) / 2);
-    const value result = predicate(half);
+  bool binary_search(int begin, int end,
+                     const std::function<value(int)>& predicate) {
+    if (begin < end) {
+      const int half = std::floor((begin + end) / 2);
+      const value result = predicate(half);
 
-    switch (result) {
-      case value::found: {
-        // Value found.
-        return true;
+      switch (result) {
+        case value::found: {
+          // Value found.
+          return true;
+        }
+        case value::to_begin: {
+          // We checked the current one, so go towards the begin.
+          return binary_search(begin, half - 1, predicate);
+        }
+        case value::to_end: {
+          // We checked the current one, so go towards the end.
+          return binary_search(half + 1, end, predicate);
+        }
+        default: {
+          throw(std::logic_error("Predicate returned unexpected value!"));
+        }
       }
-      case value::to_begin: {
-        // We checked the current one, so go towards the begin.
-        return binary_search(begin, half - 1, predicate);
-      }
-      case value::to_end: {
-        // We checked the current one, so go towards the end.
-        return binary_search(half + 1, end, predicate);
-      }
-      default: {
-        throw(std::logic_error("Predicate returned unexpected value!"));
-      }
+    } else {
+      // Value not found.
+      return false;
     }
-  } else {
-    // Value not found.
-    return false;
   }
-}
 
 }  // namespace search_matrix
 
