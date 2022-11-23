0708a4ecf5c3b53d12db650ac056efed90892b77
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-11-23 19:15:20 +0100

    move app to separate file

--- javascript/experiments/proxy-to-webpack-dev-server/client.js
+++ javascript/experiments/proxy-to-webpack-dev-server/client.js
@@ -1,26 +1,7 @@
-import { StrictMode, useState } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-const increment = 1;
-const decrement = -1;
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  function changeBy(value) {
-    return () => setCount((previousCount) => previousCount + value);
-  }
-
-  return (
-    <div>
-      <p>Wohooo! My interactive piece of web page!</p>
-      <p>HMR works as well! Cool!</p>
-      <button onClick={changeBy(increment)}>Increment</button>
-      <button onClick={changeBy(decrement)}>Decrement</button>
-      <p>The current count is: {count}</p>
-    </div>
-  );
-}
+import { App } from './App';
 
 const root = document.querySelector('#my_root');
 
