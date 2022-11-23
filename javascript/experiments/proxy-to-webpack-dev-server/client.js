fc39281bf0eed3a4ff178b3af9fae19932316ebf
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-11-23 19:16:21 +0100

    provide reference to why hmr fails on index.js

--- javascript/experiments/proxy-to-webpack-dev-server/client.js
+++ javascript/experiments/proxy-to-webpack-dev-server/client.js
@@ -5,6 +5,7 @@ import { App } from './App';
 
 const root = document.querySelector('#my_root');
 
+// https://github.com/pmmmwh/react-refresh-webpack-plugin/issues/177#issuecomment-718271457
 createRoot(root).render(
   <StrictMode>
     <App />
