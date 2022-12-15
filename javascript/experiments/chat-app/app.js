4a4a2b3441e13125fbdda45d4fa8b7c291586325
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-12-15 16:14:00 +0100

    render a page

--- javascript/experiments/chat-app/app.js
+++ javascript/experiments/chat-app/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (_, res) => {
