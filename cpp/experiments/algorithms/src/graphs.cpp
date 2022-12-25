ae1c513fa64e7e33246f5959bb85e24f7adca05a
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-12-25 15:43:38 +0100

    print connected vertices

--- cpp/experiments/algorithms/src/graphs.cpp
+++ cpp/experiments/algorithms/src/graphs.cpp
@@ -1,22 +1,31 @@
 #include <array>
 #include <iostream>
 
-int main() {
+namespace graphs {
+
+void print_connected_vertices() {
   const std::size_t side_length = 5;
   std::array<std::array<int, side_length>, side_length> graph{{
-      {0, 1, 1, 1, 1},
-      {1, 0, 1, 1, 1},
-      {1, 1, 0, 1, 1},
-      {1, 1, 1, 0, 1},
-      {1, 1, 1, 1, 0},
+      {0, 1, 1, 1, 0},
+      {1, 0, 0, 1, 0},
+      {1, 0, 0, 0, 1},
+      {0, 1, 0, 0, 1},
+      {0, 0, 1, 1, 0},
   }};
 
-  for (const auto &row : graph) {
-    for (const auto &column : row) {
-      std::cout << column << ' ';
+  for (std::size_t i = 0; i != graph.size(); ++i) {
+    for (std::size_t j = 0; j != graph.at(i).size(); ++j) {
+      if (graph.at(i).at(j) != 0) {
+        std::cout << i << " - " << j << '\n';
+      }
     }
-    std::cout << '\n';
   }
+}
+
+};  // namespace graphs
+
+int main() {
+  graphs::print_connected_vertices();
 
   return 0;
 }
