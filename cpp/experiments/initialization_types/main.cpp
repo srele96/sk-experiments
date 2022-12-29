e3888bff8089d28a8964da9ba10c8667e0ffa2c9
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-12-29 13:27:05 +0100

    finish research

--- cpp/experiments/initialization_types/main.cpp
+++ cpp/experiments/initialization_types/main.cpp
@@ -20,7 +20,31 @@ class Foo {
   }
 };
 
+Foo return_by_value() { return Foo{3}; }
+
 }  // namespace initialization_types
+
+// ----------------------------------------------------------------------------
+// Why do we need copy constructor?
+//  - https://en.wikipedia.org/wiki/Copy_constructor_(C%2B%2B)
+//
+// We have to define constructor/copy/move/destructor semantics when we own a
+// non trivial resource like:
+//  - file handle
+//  - pointer
+//  - socket
+//
+// Why?
+//
+// Compiler defines a default semantic for trivial values, however when we use a
+// resource like socket, how can a compiler know how to copy/move/destroy it?
+// Usually when we own a resource, for example a socket, we want to define how
+// to copy/move/destroy it as well. Why is that? RAII - Resource Acquisition Is
+// Initialization. As Bjarne Stroustrup said, we are good at taking ownership
+// but we are not good at cleaning up. We often forget to clean up, an error may
+// prevent cleanup or an early return. However when we define copy/move/destroy
+// at the moment of acquisition we save ourselves from these cases.
+// ----------------------------------------------------------------------------
 
 int main() {
   using Foo = initialization_types::Foo;
@@ -33,6 +57,8 @@ int main() {
   // Copy constructor is invoked when the type is declared first. That is
   // construction. Therefore we need copy constructor.
   Foo foo_five = foo_three;
+
+  initialization_types::return_by_value();
 
   return 0;
 }
