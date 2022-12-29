ce3823f8ad0c90b9ded6aebc7fc55b57377f7cc0
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-12-29 15:13:54 +0100

    get children indexes recursively

--- cpp/experiments/algorithms/src/graphs.cpp
+++ cpp/experiments/algorithms/src/graphs.cpp
@@ -33,6 +33,25 @@ children_indexes get_children_indexes(int index) {
   return children_indexes{2 * index + 1, 2 * index + 2};
 }
 
+// Recursion practise. I spotted a chance to write a recursive function even
+// though it is much more robust than the formula. I noticed that the indexes of
+// left and right child for N-th node start from the right child's index of the
+// N-1-th node. I wanted to write a recursive function for that.
+children_indexes get_children_indexes_recursively(int index) noexcept {
+  // I didn't learn error handling yet so you better pass this function a
+  // positive integer.
+  if (index == 0) {
+    return children_indexes{1, 2};
+  }
+
+  const children_indexes previous = get_children_indexes_recursively(index - 1);
+  const int left_child = 1;
+  const int right_child = 2;
+
+  return children_indexes{previous.right + left_child,
+                          previous.right + right_child};
+}
+
 };  // namespace data_structure
 
 namespace graphs {
@@ -59,12 +78,16 @@ void print_connected_vertices() {
 };  // namespace graphs
 
 int main() {
-  const int index = 1;
+  const int index = 2;
   data_structure::children_indexes children_indexes =
       data_structure::get_children_indexes(index);
+  data_structure::children_indexes children_indexes_recursively =
+      data_structure::get_children_indexes_recursively(index);
 
   std::cout << "Child " << children_indexes.left << " , "
             << children_indexes.right << '\n';
+  std::cout << "Child indexes recursively " << children_indexes_recursively.left
+            << " , " << children_indexes_recursively.right << '\n';
 
   graphs::print_connected_vertices();
 
