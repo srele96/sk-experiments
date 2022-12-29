460dc1a7aef2282abd9e13b3a79c1d7425b12dc8
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-12-29 12:46:47 +0100

    remove strict flags
    Strict flags hinder me from running code however i wish because i am
    trying to learn concept.

--- cpp/experiments/initialization_types/run.bat
+++ cpp/experiments/initialization_types/run.bat
@@ -1,3 +1,3 @@
 @REM take the filename of the c++ program with main function, compile and run it
 mkdir build
-clang++ -Wall -Wextra -Werror -Wshadow -Weffc++ -Wnon-virtual-dtor -pedantic -std=c++17 %1.cpp -o build\%1.exe && build\%1
+clang++ -std=c++17 %1.cpp -o build\%1.exe && build\%1
