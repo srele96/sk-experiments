79673a61f658000b4c42a217844b31c1e7102de3
srele96 <srele96@gmail.com>
2023-01-05 22:20:35 +0100

    fill, print and empty the min heap

--- cpp/experiments/algorithms/src/try_priority_queue.cpp
+++ cpp/experiments/algorithms/src/try_priority_queue.cpp
@@ -1,5 +1,7 @@
+#include <functional>
 #include <iostream>
 #include <queue>
+#include <vector>
 
 int main() {
   std::priority_queue<int> max_heap;
@@ -13,6 +15,19 @@ int main() {
   // Print and empty the heap
   for (; !max_heap.empty(); max_heap.pop()) {
     std::cout << "Pop: " << max_heap.top() << '\n';
+  }
+
+  std::priority_queue<int, std::vector<int>, std::less<int>> min_heap;
+
+  // Fill the heap with unordered numbers
+  for (int number : {3, 2, 1, 5, 10, 7}) {
+    std::cout << "Push: " << number << '\n';
+    min_heap.push(number);
+  }
+
+  // Print and empty the heap
+  for (; !min_heap.empty(); min_heap.pop()) {
+    std::cout << "Pop: " << min_heap.top() << '\n';
   }
 
   return 0;
