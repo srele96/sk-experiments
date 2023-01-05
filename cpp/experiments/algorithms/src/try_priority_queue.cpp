d5ea12b10d5737af1e58c93e128b317e1a1d312e
srele96 <srele96@gmail.com>
2023-01-05 22:11:14 +0100

    fill, print and empty the heap

---
+++ cpp/experiments/algorithms/src/try_priority_queue.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+
+int main() {
+  std::priority_queue<int> max_heap;
+
+  // Fill the heap with unordered numbers
+  for (const int number : {3, 2, 1, 5, 10, 7}) {
+    std::cout << "Push: " << number << '\n';
+    max_heap.push(number);
+  }
+
+  // Print and empty the heap
+  for (; !max_heap.empty(); max_heap.pop()) {
+    std::cout << "Pop: " << max_heap.top() << '\n';
+  }
+
+  return 0;
+}
