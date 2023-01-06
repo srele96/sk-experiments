accaa4d21eed850862cf9bf33164d775b8e9348e
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2023-01-07 00:05:21 +0100

    implement iterative tower of hanoi using stack

---
+++ cpp/experiments/algorithms/src/stack_tower_of_hanoi.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+namespace data {
+
+struct tower {
+  char from;
+  char end;
+  char aux;
+};
+
+}  // namespace data
+
+namespace algorithm {
+
+struct function_call {
+  int move;
+
+  data::tower tower;
+};
+
+// Notes:
+//
+// - I am aware the algorithm is copying resources. I will learn how to handle
+// that eventually, my current priority are algorithms.
+//
+// - The algorithm has notable difference to the recursive version. It doesn't
+// behave exactly in the same way recursion does. For each last call it
+// sequentially executes the three call_stack.push() calls.
+//
+// - Another notable difference to the recursive version is that the order of
+// calls is reversed.
+//
+// - One more notable difference is that it requires me to explicitly do
+// call_stack.push({1, { /* ... */ }}) that invokes the base case.
+
+void tower_of_hanoi(int moves, data::tower tower) {
+  std::stack<function_call> call_stack;
+  call_stack.push({moves, tower});
+
+  while (!call_stack.empty()) {
+    const function_call last_call = call_stack.top();
+    call_stack.pop();
+
+    if (last_call.move == 1) {
+      std::cout << "Move disk " << last_call.move << " from "
+                << last_call.tower.from << " to " << last_call.tower.end
+                << '\n';
+    } else {
+      const data::tower aux_to_end{last_call.tower.aux, last_call.tower.end,
+                                   last_call.tower.from};
+      call_stack.push({last_call.move - 1, aux_to_end});
+
+      const data::tower from_to_end{last_call.tower.from, last_call.tower.end,
+                                    last_call.tower.aux};
+      call_stack.push({1, from_to_end});
+
+      const data::tower from_to_aux{last_call.tower.from, last_call.tower.aux,
+                                    last_call.tower.end};
+      call_stack.push({last_call.move - 1, from_to_aux});
+    }
+  }
+}
+
+}  // namespace algorithm
+
+int main() {
+  const int three_moves = 3;
+  const data::tower tower{'A', 'C', 'B'};
+
+  std::cout << "Moves = " << three_moves << " \n";
+  algorithm::tower_of_hanoi(three_moves, tower);
+
+  std::cout << '\n';
+
+  const int two_moves = 2;
+  std::cout << "Moves = " << two_moves << " \n";
+  algorithm::tower_of_hanoi(two_moves, tower);
+
+  return 0;
+}
