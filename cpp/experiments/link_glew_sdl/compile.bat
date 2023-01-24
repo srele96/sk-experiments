5bd231d1f19cc10a1c9d49f9a9fa14c55e41f010
srele96 <srele96@gmail.com>
2023-01-24 23:20:38 +0100

    update compilation command
    I can place .lib files in the deps/<library_name>/lib to sort them in a
    bettery way. Therefore I specified those in the include paths.

--- cpp/experiments/link_glew_sdl/compile.bat
+++ cpp/experiments/link_glew_sdl/compile.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-clang++ main.cpp -L./ -lSDL2main -lSDL2 -lglew32 -lopengl32 -I./deps/GLEW-2.2.0/include -I./deps/SDL2-2.26.2/include -o main
+clang++ main.cpp -L./deps/GLEW-2.2.0/lib -L./deps/SDL2-2.26.2/lib -lSDL2main -lSDL2 -lglew32 -lopengl32 -I./deps/GLEW-2.2.0/include -I./deps/SDL2-2.26.2/include -o main
