fb3163fced1573166b47fe717c46439beafa5c22
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2023-01-31 20:06:21 +0100

    create record type
    To completely understand implementation decisions of the TypeScript
    utility Record<Type, Keys>. Just as Richard Feynman said, if I cannot
    create it, I do not understand it.

--- javascript/experiments/play-with-types/src/index.ts
+++ javascript/experiments/play-with-types/src/index.ts
@@ -37,3 +37,66 @@ const deepOptional: DeepOptional<Keys> = {
   b: '2',
   c: {}, // Ok
 };
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+type MyRecord<T extends keyof any, E> = {
+  [K in T]: E;
+};
+
+type Mage = 'Mage';
+type Warrior = 'Warrior';
+type Rogue = 'Rogue';
+
+interface Weapon {
+  name: string;
+  damage: number;
+}
+
+interface Health {
+  current: number;
+  max: number;
+}
+
+const heroes: MyRecord<
+  Mage | Warrior | Rogue,
+  { weapon: Weapon; health: Health }
+> = {
+  Mage: {
+    weapon: {
+      name: 'Staff',
+      damage: 10,
+    },
+    health: {
+      current: 100,
+      max: 100,
+    },
+  },
+  Warrior: {
+    weapon: {
+      name: 'Sword',
+      damage: 15,
+    },
+    health: {
+      current: 150,
+      max: 150,
+    },
+  },
+  Rogue: {
+    weapon: {
+      name: 'Dagger',
+      damage: 20,
+    },
+    health: {
+      current: 200,
+      max: 200,
+    },
+  },
+};
+
+// Test `keyof T`, it gives me a union of all keys of the type. So basically,
+// any time I get a `keyof any` I get a union and I can iterate over it using
+// `K in T`. However I can't accept unrestricted type because `keyof T` which
+// is not restricted isn't what I want.
+type WeaponProperties = keyof Weapon;
+const weaponProperties: WeaponProperties = 'damage';
