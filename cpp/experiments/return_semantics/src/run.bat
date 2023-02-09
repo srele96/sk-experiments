eb594054f220cb1d04f56ca7a632712392293da2
srele96 <srele96@gmail.com>
2023-02-10 00:06:40 +0100

    add reference to the solution that I've used

--- cpp/experiments/return_semantics/src/run.bat
+++ cpp/experiments/return_semantics/src/run.bat
@@ -7,6 +7,9 @@ SET out_path=%out_dir%%executable%
 
 ECHO Compiling %file_name%
 
+@REM I was getting error that the syntax is incorrect and google search gave me
+@REM this solution on how to check if directory exists.
+@REM https://stackoverflow.com/questions/21033801/checking-if-a-folder-exists-using-a-bat-file
 IF NOT EXIST %out_dir% (
   ECHO Creating %out_dir% directory...
   MKDIR %out_dir%
