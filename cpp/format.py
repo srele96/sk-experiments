5217ae7de926ea7148bc66441f8c92e6a42b19a2
srele96 <srele96@gmail.com>
2023-02-10 02:14:23 +0100

    move formatting documentation to python file
    It will be very easy for me to forget to update documentation if I
    change the functionality. However keeping it close to the implementation
    will make that error less possible.

--- cpp/format.py
+++ cpp/format.py
@@ -46,6 +46,29 @@ class Formatter:
             print('Formatting file: ' + file)
             os.system('clang-format -i -style=file ' + file)
 
+"""
+# Format
+
+Documentation on how to format using `format.py`.
+
+## Requirements
+
+Python installed. I am using version 3.10 at the moment of writing this documentation.
+
+## Instructions
+
+Format c++ code in this directory:
+
+```terminal
+python format.py
+```
+
+Check formatting of the C++ code in this directory:
+
+```terminal
+python format.py --check
+```
+"""
 def run_formatter():
     formatter = Formatter()
     if '--check' in sys.argv:
