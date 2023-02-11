7981484a15d28e4fbc6042f3e98290a317266e74
srele96 <srele96@gmail.com>
2023-02-12 00:06:10 +0100

    setup boost to work with cmake

---
+++ cpp/experiments/try_boost_asio/src/main.cpp
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+
+int main() {
+  using namespace boost::lambda;
+  typedef std::istream_iterator<int> in;
+
+  std::for_each(in(std::cin), in(), std::cout << (_1 * 3) << " ");
+}
