ed8d626e1a56cdcc9e7cad012a752eb70fed1c84
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2023-02-24 18:40:37 +0100

    rename components to component
    The other directories use singularity.

--- javascript/experiments/maze/src/entry.js
+++ javascript/experiments/maze/src/entry.js
@@ -1,6 +1,6 @@
 import { createElement as e, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './components/App';
+import { App } from './component/App';
 
 function renderApp() {
   const id = 'app';
