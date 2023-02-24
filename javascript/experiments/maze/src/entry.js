0b2dc3a938384a743a03514eae4d55b7d59eb412
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2023-02-24 18:41:16 +0100

    add empty space

--- javascript/experiments/maze/src/entry.js
+++ javascript/experiments/maze/src/entry.js
@@ -5,6 +5,7 @@ import { App } from './component/App';
 function renderApp() {
   const id = 'app';
   const domRoot = document.getElementById(id);
+
   createRoot(domRoot).render(e(StrictMode, null, e(App)));
 }
 
