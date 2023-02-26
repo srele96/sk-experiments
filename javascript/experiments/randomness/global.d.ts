937c589d55b7c76fec6b5d3822f4c2332d708016
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2023-02-26 15:00:53 +0100

    use react, react-dom, and my lib with typescript

---
+++ javascript/experiments/randomness/global.d.ts
@@ -0,0 +1,2 @@
+// Global type declaration for libraries without type declarations.
+/// <reference path="./@types/@flexos/elements/index.d.ts" />
---
+++ javascript/experiments/randomness/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+const config = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: 'public',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['...','.ts'],
+  },
+};
+
+module.exports = config;
