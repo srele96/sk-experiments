d12ab71c8053ce969b903c243c604a71a0f5a3c5
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2023-05-27 17:28:17 +0200

    format class_friend.cpp

--- cpp/experiments/algorithms/src/class_friend.cpp
+++ cpp/experiments/algorithms/src/class_friend.cpp
@@ -28,17 +28,19 @@ void besto_friendo(Subject& subject);
 
 int main() {
   constexpr Subject::something non_magic{20};
-  std::cout << non_magic << '\n'; // Shut up linter!
+
+  std::cout << non_magic << '\n';  // Shut up linter!
+
   // Interesting... a type can be used when class acts as a namespace. An
   // integer can't be used through class name.
   // std::cout << Subject::whatever;
 
   Subject access_type;
+
   // Makes sense. I can't access the type through the object.
   // access_type::something magic{30};
 
   besto_friendo(access_type);
-  // Obscure.
 
   return 0;
 }
