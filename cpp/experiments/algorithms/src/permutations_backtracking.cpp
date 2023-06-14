2531ffafe6a15abb2cf089d07b9e8fa2d75c4d30
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2023-06-14 23:20:26 +0200

    add comment of my thought process

--- cpp/experiments/algorithms/src/permutations_backtracking.cpp
+++ cpp/experiments/algorithms/src/permutations_backtracking.cpp
@@ -1,6 +1,130 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+
+
+1
+
+[]
+[1]
+
+
+1 2
+
+[]
+[1]
+[1 2] -> done
+[1]
+[]
+[2]
+[2 1] -> done
+
+
+1 2 3
+
+[]
+[1]
+[1 2]
+[1 2 3] -> done
+[1 2]
+[1]
+[1 3]
+[1 3 2] -> done
+[1 3]
+[1]
+[]
+[2]
+[2 1]
+[2 1 3] -> done
+[2 1]
+[2]
+[2 3]
+[2 3 1] -> done
+[2 3]
+[2]
+[]
+[3]
+[3 1]
+[3 1 2] -> done
+[3 1]
+[3]
+[3 2]
+[3 2 1] -> done
+
+
+1 2 3 4
+
+[]
+[1]
+[1 2]
+[1 2 3]
+[1 2 3 4] -> done
+[1 2 3]
+[1 2]
+[1 2 4]
+[1 2 4 3] -> done
+[1 2 4]
+[1 2]
+[1]
+[1 3]
+[1 3 2]
+[1 3 2 4] -> done
+[1 3 2]
+[1 3]
+[1 3 4]
+[1 3 4 2] -> done
+[1 3 4]
+[1 3]
+[1]
+[1 4]
+[1 4 2]
+[1 4 2 3] -> done
+[1 4 2]
+[1 4]
+[1 4 3]
+[1 4 3 2] -> done
+[1 4 3]
+[1 4]
+[1]
+[]
+[2]
+
+
+
+
+
+for each number
+  if number is not in current
+    add it to the current
+    backtrack to current
+    remove the current
+
+
+
+
+
+
+
+1 2
+
+[]
+for each number
+  if number is not currently
+    add it
+    repeat for others
+    remove it
+
+1 2 3 4 5
+
+[]
+[1]
+repeat for others
+remove it
+
+
+*/
+
 class Solution {
  private:
   void backtrack(vector<vector<int>>& answer, const vector<int>& nums,
