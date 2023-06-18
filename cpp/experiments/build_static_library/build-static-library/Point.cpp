78e0065a858688003bb9cdaa6001437e49171270
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2023-06-18 18:00:36 +0200

    chore: remove accidental files (#245)

--- cpp/experiments/build_static_library/build-static-library/Point.cpp
+++
@@ -1,5 +0,0 @@
-#include "Point.hpp"
-
-Point::Point() : x_coordinate(0), y_coordinate(0){};
-Point::Point(const float x_coordinate, const float y_coordinate)
-    : x_coordinate(x_coordinate), y_coordinate(y_coordinate){};
