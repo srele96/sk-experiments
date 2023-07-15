d99fe088749730d2c3a00f1c961725342b8f5472
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2023-07-15 20:36:21 +0200

    change ownership hierarchy

--- cpp/experiments/random_stuff/curry.cpp
+++ cpp/experiments/random_stuff/curry.cpp
@@ -6,23 +6,25 @@ int main() {
   // that Bjarne wanted C++ to support useful programming patterns. In his time
   // there was no Object Oriented Programming language as efficient as C.
 
+  // Can store state in the object and use it in the operator() function.
   struct Curry {
-    // Can store state in the object and use it in the operator() function.
-
-    struct A {
-      void operator()(int x, int y) const {
-        std::cout << "A: (" << x << ", " << y << ")\n";
-      }
-    };
-
-    struct B {
-      A operator()(int x, int y) const {
-        std::cout << "B: (" << x << ", " << y << ")\n";
-        return A{};
-      }
-    };
-
+    // Only struct Curry knows about struct C because only it uses it.
     struct C {
+      // Only struct C knows about struct B because only it uses it.
+      struct B {
+        // Only struct B knows about struct A because only it uses it.
+        struct A {
+          void operator()(int x, int y) const {
+            std::cout << "A: (" << x << ", " << y << ")\n";
+          }
+        };
+
+        A operator()(int x, int y) const {
+          std::cout << "B: (" << x << ", " << y << ")\n";
+          return A{};
+        }
+      };
+
       B operator()(int x, int y) const {
         std::cout << "C: (" << x << ", " << y << ")\n";
         return B{};
@@ -38,8 +40,8 @@ int main() {
   Curry{}(1, 2)(3, 4)(5, 6)(7, 8);
 
   Curry::C c{Curry{}(9, 10)};
-  Curry::B b{c(11, 12)};
-  Curry::A a{b(13, 14)};
+  Curry::C::B b{c(11, 12)};
+  Curry::C::B::A a{b(13, 14)};
   a(15, 16);
 
   return 0;
