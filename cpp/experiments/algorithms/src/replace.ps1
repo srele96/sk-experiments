3000590f710c88e4a2930c4255158414ee434422
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2023-08-29 00:27:54 +0200

    move definitions

--- cpp/experiments/algorithms/src/replace.ps1
+++ cpp/experiments/algorithms/src/replace.ps1
@@ -14,11 +14,10 @@ param (
 # replace.bat -infile <input file> -outfile <output file>
 
 $inFilePath = Join-Path $pwd $InFile
-$outFilePath = Join-Path $pwd $OutFile
-
 $content = Get-Content -Path $inFilePath -Raw
 
 $content = $content -replace 'ChatGPT', "`n---`n`nChatGPT"
 $content = $content -replace 'User', "`n---`n`nUser`n"
 
+$outFilePath = Join-Path $pwd $OutFile
 $content | Set-Content -Path $outFilePath
