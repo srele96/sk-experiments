fa9d3febfdbe395159f4b6ed2dd699c906ad9a5e
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2023-09-20 22:20:46 +0200

    stuff...

--- javascript/experiments/practice-jest/webpack.config.js
+++ javascript/experiments/practice-jest/webpack.config.js
@@ -1,5 +1,33 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+/**
+ * The class to try out a few things with the webpack plugins. I've read that
+ * creating webpack plugins require users to read the webpack code to understand
+ * the compilation process.
+ */
+class LearnPlugin {
+  pluginName = 'LearnPlugin';
+
+  apply(compiler) {
+    compiler.hooks.done.tap(this.pluginName, (stats) => {
+      // console.log('LearnPlugin: done');
+    });
+    compiler.hooks.emit.tap(this.pluginName, (compilation) => {
+      // console.log({ assets: Object.keys(compilation.assets) });
+      // console.log({ module });
+    });
+    compiler.hooks.emit.tapAsync(this.pluginName, (compilation, callback) => {
+      Object.keys(compilation.assets).forEach((assetName) => {
+        if (assetName.endsWith('.js')) {
+          const assetContent = compilation.assets[assetName].source();
+          // console.log({ assetName, assetContent });
+        }
+      });
+      callback();
+    });
+  }
+}
+
 /** @type {import('webpack').Configuration} */
 const config = {
   entry: './entry.js',
@@ -19,6 +47,7 @@ const config = {
       template: './index.html',
       inject: 'body',
     }),
+    new LearnPlugin(),
   ],
 };
 
