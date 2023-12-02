4684a32d650c45c1decfc6f1ebbc1f16d8efeda8
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2023-12-02 21:14:07 +0100

    add some code

--- cpp/experiments/cpp_python_binding/example.cpp
+++ cpp/experiments/cpp_python_binding/example.cpp
@@ -1,11 +1,47 @@
-#include "pybind11/pybind11.h"
+#include <functional>
+#include <thread>
+#include <vector>
 
-// namespace py = pybind11;
+#include "pybind11/functional.h"
+#include "pybind11/pybind11.h"
+#include "pybind11/stl.h"
 
 int add(int i, int j) { return i + j; }
+
+void process_data(int data) {
+  constexpr int seconds{1};
+  std::this_thread::sleep_for(std::chrono::seconds(seconds));
+  data *= 2;
+}
+
+std::vector<int> threaded_operation(std::vector<int> data) {
+  std::vector<std::thread> threads;
+
+  for (int& value : data) {
+    threads.emplace_back(process_data, std::ref(value));
+  }
+
+  for (auto& thread : threads) {
+    thread.join();
+  }
+
+  return data;
+}
+
+void each_async(std::vector<int> data, std::function<void(int)> callback) {
+  for (const int value : data) {
+    constexpr int one{1};
+    std::this_thread::sleep_for(std::chrono::seconds(one));
+
+    constexpr int magnifier{2};
+    callback(value * magnifier);
+  }
+}
 
 PYBIND11_MODULE(example, m) {
   m.doc() = "pybind11 example plugin";  // optional module docstring
 
-  m.def("add", &add, "A function which adds two numbers");
+  m.def("add", &add);
+  m.def("threaded_operation", &threaded_operation);
+  m.def("each_async", &each_async);
 }
--- cpp/experiments/cpp_python_binding/something.py
+++ cpp/experiments/cpp_python_binding/something.py
@@ -2,6 +2,7 @@ import os
 import sys
 import ctypes
 
+# Allow built modules to be imported
 sys.path.append(os.path.join(os.path.dirname(__file__), 'build'))
 
 # debug the python dll module using dependency walker and if it's not
@@ -20,3 +21,11 @@ ctypes.CDLL(os.path.join(os.path.dirname(__file__), 'build', 'example.pyd'))
 import example
 
 print(example.add(1, 2))
+print(example.threaded_operation([1, 2, 3, 4, 5]))
+example.each_async([1, 2, 3, 4, 5], lambda x: print(x))
+
+try:
+    # Doesn't support non integer values, should throw.
+    example.each_async([1.25, 2.25, 3.35], lambda x: print(x))
+except TypeError as e:
+    print(e)
