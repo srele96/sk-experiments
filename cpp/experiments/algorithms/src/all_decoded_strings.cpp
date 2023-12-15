a0249a6ef931a7ab97fd94eba4133f336de1c1b0
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2023-12-15 19:53:59 +0100

    add comments

--- cpp/experiments/algorithms/src/all_decoded_strings.cpp
+++ cpp/experiments/algorithms/src/all_decoded_strings.cpp
@@ -5,13 +5,39 @@ namespace a {
 
 class Solution {
  public:
+  /**
+   * Whether it's a leading zero or somewhere within the encoded string it's a
+   * problem so we have to guard against it.
+   */
   bool canDecodeOne(const string& encoded) { return encoded != "0"; }
 
+  /**
+   * When the algorithm attempts to decode '34' as two, it will fail. This guard
+   * is necessary against invalid combinations of two encoded digits.
+   */
   bool canDecodeTwo(const string& encoded) {
     return encoded[0] == '1' || (encoded[0] == '2' && encoded[1] != '7' &&
                                  encoded[1] != '8' && encoded[1] != '9');
   }
 
+  /**
+   * The algorithm produces all valid decoded strings.
+   *
+   * The recursion states:
+   *
+   * If we have only one character, decode it.
+   * If we have two characters, decode it:
+   *  - Combine each decoded one character.
+   *  - Decode as two
+   * For each n-1th decoded string:
+   *  - Decode from start until the last digit
+   *  - Take the last digit and decode it
+   *  - Merge decoded start and last digit
+   * For each n-2th decoded string:
+   *  - Decode from start until the last two digits
+   *  - Take the last two digits and decode them
+   *  - Merge decoded start and last two digits
+   */
   vector<string> decodeAll(const string& encoded,
                            const unordered_map<string, string>& decode) {
     if (encoded.size() == 1) {
@@ -52,6 +78,9 @@ class Solution {
   }
 
   int numDecodings(string s) {
+    /**
+     * Build the map for digit decodings.
+     */
     unordered_map<string, string> decode{[] {
       unordered_map<string, string> decode;
       int n = 1;
