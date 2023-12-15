1f9861c6d3383bc7c3527770b77bfec375c5ba63
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2023-12-15 01:48:28 +0100

    remove unused import

--- javascript/experiments/styled/server.js
+++ javascript/experiments/styled/server.js
@@ -3,7 +3,7 @@ import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 
-import { createElement as e, Fragment } from 'react';
+import { createElement as e } from 'react';
 import { renderToString } from 'react-dom/server';
 import { ServerStyleSheet } from 'styled-components';
 import { App } from './App';
